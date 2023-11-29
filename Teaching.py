33eff2965b28a6a22787df1009587d18527fe60b
Dennis Ramirez <38735947+RTL-TPT@users.noreply.github.com>
2023-11-29 16:46:43 -0600

    Works with 3 clues
    Updated the solver to ask you for 3 clues. If there are no matches given a clue, it says there are no matches and kicks you out.

--- Teaching.py
+++ Teaching.py
@@ -7,7 +7,35 @@ df = pd.read_csv(csvFile)
 firstObject = str(input("What do you put on the white board?")).lower().title()
 print(firstObject)
 
+count = 0
 for index, row in df.iterrows():
-    categoryName = str(row['Category'])
-    if str(row[firstObject]) == '1':
-	    print("Is it " + str(categoryName)+ "?")
+	categoryName = str(row['Category'])
+	if str(row[firstObject]) == '1':
+		print("Is it " + str(categoryName)+ "?")
+		count = count + 1
+
+secondObject = str(input("What do you put on the white board next?")).lower().title()
+
+count = 0
+for index, row in df.iterrows():
+	categoryName = str(row['Category'])
+	if str(row[firstObject]) == '1' and str(row[secondObject])=='1':
+		print("Is it " + str(categoryName)+ "?")
+		count = count + 1
+	
+if(count<1):
+	print("There are no results for these combinations")
+	exit(0)
+
+thirdObject = str(input("What do you put on the white board next?")).lower().title()
+
+count = 0
+for index, row in df.iterrows():
+	categoryName = str(row['Category'])
+	if str(row[firstObject]) == '1' and str(row[secondObject])== '1' and  str(row[secondObject]) == '1':
+		print("Is it " + str(categoryName)+ "?")
+		count = count + 1
+	
+if(count<1):
+	print("There are no results for these combinations")
+	exit(0)
